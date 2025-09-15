8ef1d8e0eabb1f51eb6a8715e7da08214f27e2c9
billza555 <nphisu@gmail.com>
2025-09-15 16:40:01 +0700

    ex02

--- cell05/ex02/play_with_arrays.py
+++ cell05/ex02/play_with_arrays.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 array_number = [2, 8, 9, 48, 8, 22, -12, 2]
 new_array_number = [x + 2 for x in array_number if x > 5]
-print(f"Original array: {array_number}")
-print(f"New array: {new_array_number}")
+print(f"{array_number}")
+print(f"{new_array_number}")
