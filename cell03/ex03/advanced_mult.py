a78b53615cd1027d96efcfbe994d56c22a677b6f
billza555 <nphisu@gmail.com>
2025-09-15 15:04:49 +0700

    ex03

---
+++ cell03/ex03/advanced_mult.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import sys
+
+if len(sys.argv) == 1:
+    for i in range(11):
+        print(f"Table de {i}: ", end = "")
+        for j in range(11):
+            print(f"{i * j}", end = " ")
+        print()
+else:
+    print("none")
